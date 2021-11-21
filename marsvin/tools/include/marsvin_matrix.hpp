92257dc84bfb33cb9a157323d87e73d71e19896c
Julius Diestra <julius.diestra.cabrera@gmail.com>
2021-11-21 08:55:49 +0100

    Fix in template definition

--- marsvin/tools/include/marsvin_matrix.hpp
+++ marsvin/tools/include/marsvin_matrix.hpp
@@ -22,7 +22,24 @@ class Matrix {
         std::size_t column_;
         std::vector<T> data_;
 };
+
+// Implementation
+template<typename T> marsvin::Matrix<T>::Matrix(std::size_t row, std::size_t column): row_{ row }, column_{ column }, data_{ std::vector<T>(row*column,0) } {}
+template<typename T> marsvin::Matrix<T>::Matrix():Matrix(0,0) {}
+template<typename T> marsvin::Matrix<T>::Matrix(std::size_t n):Matrix(n,n){}
+template<typename T> void marsvin::Matrix<T>::SetValue(std::size_t row, std::vector<T> data) {
+    data_.insert(data_.begin()+row,data.begin(),data.end());
 }
+template<typename T> std::size_t marsvin::Matrix<T>::GetNumberOfRows() {
+    return row_;
+}
+
+template<typename T> std::size_t marsvin::Matrix<T>::GetNumberOfColumns() {
+    return column_;
+};
+
+}
+
 
 #endif // MARSVIN_MATRIX_HPP_
 
--- marsvin/tools/src/matrix.cpp
+++ marsvin/tools/src/matrix.cpp
@@ -1,11 +1,16 @@
 #include "marsvin_matrix.hpp"
 
-// Constructors
-marsvin::Matrix::Matrix(std::size_t row, std::size_t column): row_{ row }, colum_{ column }, data_{ std::vector<T>(row*column,0) } {};
-marsvin::Matrix::Matrix():Matrix(0,0) {};
-marsvin::Matrix::Matrix(std::size_t n):Matrix(n,n){};
+template class marsvin::Matrix<int>;
+template class marsvin::Matrix<float>;
+template class marsvin::Matrix<double>;
 
-void marsvin::Matrix::SetValue(std::size_t row, std::vector<T> data) {
+/*
+// Constructors
+template<typename T> marsvin::Matrix<T>::Matrix(std::size_t row, std::size_t column): row_{ row }, column_{ column }, data_{ std::vector<T>(row*column,0) } {};
+template<typename T> marsvin::Matrix<T>::Matrix(): Matrix(0,0) {};
+template<typename T> marsvin::Matrix<T>::Matrix(std::size_t n):Matrix(n,n){};
+
+template<typename T> void marsvin::Matrix<T>::SetValue(std::size_t row, std::vector<T> data) {
     data_.insert(data_.begin()+row,data.begin(),data.end());
 };
 
@@ -16,5 +21,4 @@ std::size_t marsvin::Matrix::GetNumberOfRows() {
 std::size_t marsvin::Matrix::GetNumberOfColumns() {
     return column_;
 };
-
-
+*/
