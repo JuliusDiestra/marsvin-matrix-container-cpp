aff96bb92ecd829441fb725f7cdab34c472d0e19
jdiestra <julius.diestra.cabrera@gmail.com>
2022-05-02 23:21:32 +0200

    Adding fixes in marsvin logger class.

--- core/include/marsvin_logger.hpp
+++ core/include/marsvin_logger.hpp
@@ -19,10 +19,28 @@ namespace marsvin {
  */
 class Logger {
     public:
+        /*
+         * @brief Default constructor.
+         *
+         * Creates Logger instance use to stream marsvin::Matrix and std::vector elements.
+         *
+         */
         Logger();
-        template<typename T> static void operator<<(const marsvin::Matrix<T>& matrix);
-        template<typename T> static void operator<<(const std::vector<T>& vector);
-}
+        /*
+         * @brief Method to print marsvin::Matrix elements.
+         *
+         * @param matrix Matrix instance to stream elements.
+         *
+         */
+        template<typename T> void operator<<(const marsvin::Matrix<T>& matrix);
+        /*
+         * @brief Method to print std::vector elements.
+         *
+         * @param vector Vector instance to stream elements.
+         *
+         */
+        template<typename T> void operator<<(const std::vector<T>& vector);
+};
 
 }
 
@@ -30,20 +48,25 @@ class Logger {
 
 marsvin::Logger::Logger(){};
 
-template<typename T> void operator<<(const marsvin::Matrix<T>& matrix) {
+template<typename T> void marsvin::Logger::operator<<(const marsvin::Matrix<T>& matrix) {
+    std::vector<T> data_;
     data_ = matrix.GetVectorData();
     for (std::size_t j = 0; j < data_.size(); j++) {
         if ( (j+1) % matrix.GetNumberOfColumns() == 0) {
             std::cout << data_[j] << '\n';
         } else {
-            std::cout << data_.[j] << '\t';
+            std::cout << data_[j] << '\t';
         }
     };
 }
 
-template<typename T> void operator<<(const std::vector<T>& vector) {
-    // to be done
+template<typename T> void marsvin::Logger::operator<<(const std::vector<T>& vector) {
+    for (std::size_t j = 0; j < vector.size(); j++) {
+        if (j != vector.size() -1 ) {
+            std::cout << vector.at(j) << '\t';
+        } else {
+            std::cout << vector.at(j) << '\n';
+        }
+    }
 }
-
-
 #endif // MARSVIN_CORE_MARSVIN_LOGGER_HPP_
